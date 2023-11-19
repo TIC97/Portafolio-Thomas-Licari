5f25abc15c22d3628197672041edf3435ec35c6f
TIC97 <thomysurb1997@gmail.com>
2023-11-19 20:58:56 -0300

    Terminado revisar testimonios estilado feo

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -43,20 +43,20 @@ const Testimonials = () => {
       <h2>Testimonials</h2>
 
       <Swiper className="container testimonials__container"
-      // install Swiper modules
+      //* install Swiper modules
       modules={[ Pagination ]} spaceBetween={40}
       slidesPerView={1}
       pagination={{ clickable: true }}>
         {
-          data.map(({avatar, name, review }, index) =>{
-            return(
-              <SwiperSlide key={index} className='testimonial'>
-              <div className="client__avatar">
-                <img src={avatar} />
-              </div>
-              <h5 className='client__name'>{name}</h5>
-              <small className='client__review'>{review}</small>
-              </SwiperSlide>
+          data.map(({avatar, name, review }, index) => {
+            return (
+                <SwiperSlide key={index} className='testimonial'>
+                <div className="client__avatar">
+                  <img src={avatar} />
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>{review}</small>
+                </SwiperSlide>
             )
           })
         }
